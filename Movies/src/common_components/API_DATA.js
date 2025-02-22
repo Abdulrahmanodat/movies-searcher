28ed462319a39c1a61fd40df5a8edbf381618374
Abdulrahmanodat <abood.odat.9988@gmail.com>
2025-02-23 00:08:19 +0300

    Delete Movies directory

--- Movies/src/common_components/API_DATA.js
+++
@@ -1,6 +0,0 @@
-const data = {
-    API_URL: 'https://www.omdbapi.com',
-    API_KEY: 'a330e532'
-};
-
-export default data;
--- Movies/src/common_components/errorMessage.js
+++
@@ -1,8 +0,0 @@
-function ErrorMessage(errorMessage) {
-    errorMessage = errorMessage.errorMessage;
-    return (
-        <h1 className='errorMessage'>{ errorMessage }</h1>
-    );
-}
-
-export default ErrorMessage;
